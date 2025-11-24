90faa04aea66ca2ee813d6d2ec459dd6589fcfd5
Sicaooo <guimoura2704@gmail.com>
2025-11-24 17:48:29 -0300

    Término do modulo final junto com seu testbench e forma de onda resultante

--- final.v
+++ final.v
@@ -1,10 +1,17 @@
 `include "ULA.v"
-
-module final(in_A, in_B, clk, pr_A, pr_B, clr_A, clr_B, sel_in, out);
-    input clk, pr_A, clr_A, pr_B, clr_B;
+//pr_A, pr_B, clr_A, clr_B, pr_out, clr_out
+module final(in_A, in_B, clk, pr_A, pr_B, clr_A, clr_B, pr_out, clr_out, sel_in, out);
+    input clk, pr_A, pr_B, clr_A, clr_B, pr_out, clr_out;
     input [0:7]in_A, in_B;
     input [0:2]sel_in;
-    output[0:8]out;
+    output[0:7]out;
 
+    wire [0:7]regA, regB, regOUT;
+    registrador reg_A(in_A, clk, pr_A, clr_A, regA);
+    registrador reg_B(in_B, clk, pr_B, clr_B, regB);
+    
+    ULA ula(regA, regB, sel_in, regOUT);
+   
+    registrador reg_out(regOUT, clk, pr_out, clr_out, out);
 
 endmodule
--- final_tb.v
+++ final_tb.v
@@ -1,56 +1,63 @@
 `include "final.v"
 `timescale 1ps/1ps
 
-module ULA_tb;
-    reg clk, pr, clr;
+module final_tb;
     reg [0:7]a, b;
+    reg clk, pr, clr, pr_A, pr_B, clr_A, clr_B, pr_out, clr_out;
     reg [0:2]sel_in;
     wire [0:8]out;
 
-    final uut(a, b, sel_in, out);
+    final uut(
+        .in_A(a),
+        .pr_A(pr_A),
+        .clr_A(clr_A),
+        .in_B(b),
+        .pr_B(pr_B),
+        .clr_B(clr_B),
+        .clk(clk),
+        .sel_in(sel_in),
+        .out(out),
+        .pr_out(pr_out),
+        .clr_out(clr_out)
+    );
 
     initial begin
         $dumpfile("final_tb.vcd");
         $dumpvars(0, final_tb);
-
+        pr_A = 0;
+        pr_B = 0;
+        pr_out = 0;
+        clr_A = 1;
+        clr_B = 1;
+        clr_out = 1;
         a = 8'd10;
         b = 8'd5;
-        sel_in = 3'd0;
-        #5;
-        sel_in = 3'd1;
-        #5;
-        sel_in = 3'd2;
-        #5;
-        sel_in = 3'd3;
-        #5;
-        b = 8'd10;
-        #5;
-        b = 8'd5;
-        sel_in = 3'd4;
-        #2;
-        a = 8'd5;
-        b = 8'd10;
-        #3;
-        a = 8'd10;
-        b = 8'd5;
-        sel_in = 3'd5;
-        #2;
-        a = 8'd5;
-        b = 8'd10;
-        #3;
-        a = 8'd10;
-        b = 8'd5;
-        sel_in = 3'd6;
-        #5;
-        sel_in = 3'd7;
-        #10;
-
+        sel_in = 3'd0; // a + b
+        #6;
+        clr_A = 0;
+        clr_B = 0;
+        clr_out = 0;
+        #15;
+        sel_in = 3'd1; // a - b
+        #15;
+        sel_in = 3'd2; // a * b
+        #15;
+        sel_in = 3'd3; // a == b
+        #15;
+        sel_in = 3'd4; // a > b
+        #15;
+        sel_in = 3'd5; // a < b
+        #15;
+        sel_in = 3'd6; // a & b
+        #15;
+        sel_in = 3'd7; // a | b
+        #20;
         $finish;
     end
 
     initial begin
         clk = 0;
-        forever clk = ~clk;
+        forever #5 clk = ~clk;
     end
 
 
