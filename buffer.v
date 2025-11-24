f25b46228da77493dc813332f1d995e420dec9b1
Sicaooo <guimoura2704@gmail.com>
2025-11-24 10:27:40 -0300

    correção de problemas com a atribuição das ligações indeterminadas e adaptação para a conexão de 8 bits

--- buffer.v
+++ buffer.v
@@ -1,10 +1,10 @@
 module buffer(c1, c2, en);
     input en;
-    input [0:7]c1;
-    output [0:7]c2;
+    input [0:8]c1;
+    output reg [0:8]c2;
 
     always @(*) begin
-        if (en == 0) c2 = z;
+        if (en == 0) c2 = 9'bz;
         else c2 = c1;
     end
 
